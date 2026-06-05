1f6951d4ec7f464c9566b909c0bd6e9654fd94b9
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-04 21:58:26 -0400

    Implement Agua class extending Hechizo

--- Taller3/src/modelo/HechizoAgua.java
+++ Taller3/src/modelo/HechizoAgua.java
@@ -1,5 +1,39 @@
 package modelo;
 
-public class HechizoAgua {
+public class Agua extends Hechizo {
+	private static double cantidadHeal;
+	private static double presionDelAgua;
+
+	public Agua(String nombrehechizo, String tipohechizo, double daño,int cantidadheal,int presiondelagua) {
+		super(nombrehechizo, tipohechizo, daño);
+		// TODO Auto-generated constructor stub
+		this.cantidadHeal=cantidadheal;
+		this.presionDelAgua=presiondelagua;
+	}
+
+	public static double getCantidadHeal() {
+		return cantidadHeal;
+	}
+
+	public static double getPresionDelAgua() {
+		return presionDelAgua;
+	}
+
+	public void setCantidadHeal(int cantidadHeal) {
+		this.cantidadHeal = cantidadHeal;
+	}
+
+	public void setPresionDelAgua(int presionDelAgua) {
+		this.presionDelAgua = presionDelAgua;
+	}
+	
+
+	@Override
+	public double  accept(VisitoI visitor) {
+		// TODO Auto-generated method stub
+		return visitor.visitar(this);
+		
+	}
 
 }
+
