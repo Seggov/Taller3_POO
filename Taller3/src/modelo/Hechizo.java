8e8999abaae2c2828fafb7e64a685258d89c9b74
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-04 21:52:11 -0400

    Refactor Hechizo class to include properties and methods
    hecho clase Hechizo

--- Taller3/src/modelo/Hechizo.java
+++ Taller3/src/modelo/Hechizo.java
@@ -1,5 +1,40 @@
 package modelo;
 
-public class Hechizo {
+public abstract class Hechizo {
+	private String nombreHechizo;
+	private String tipoHechizo;
+	private static double Daño;
+	
+	public Hechizo(String nombrehechizo, String tipohechizo,double daño) {
+		this.nombreHechizo=nombrehechizo;
+		this.tipoHechizo=tipohechizo;
+		this.Daño=daño;
+	}
 
+	public static double getDaño() {
+		return Daño;
+	}
+
+	
+	public String getNombreHechizo() {
+		return nombreHechizo;
+	}
+
+	public String getTipoHechizo() {
+		return tipoHechizo;
+	}
+
+	public void setNombreHechizo(String nombreHechizo) {
+		this.nombreHechizo = nombreHechizo;
+	}
+
+	public void setTipoHechizo(String tipoHechizo) {
+		this.tipoHechizo = tipoHechizo;
+	}
+	public void setDaño(double daño) {
+		Daño = daño;
+	}
+	public abstract double accept(VisitoI visitor);
+
+ 
 }
