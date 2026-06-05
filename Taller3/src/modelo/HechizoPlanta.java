6d40d50f557589ba684b25b3af2c2de6aa2b6571
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-04 22:02:33 -0400

    Implement Planta class extending Hechizo

--- Taller3/src/modelo/HechizoPlanta.java
+++ Taller3/src/modelo/HechizoPlanta.java
@@ -1,5 +1,39 @@
 package modelo;
 
-public class HechizoPlanta {
+public class Planta extends Hechizo {
+	private static double DuracionStun;
+	private static  double CantPlantas;
+
+	public Planta(String nombrehechizo, String tipohechizo, double daño,int duracionstun,int cantplantas) {
+		super(nombrehechizo, tipohechizo, daño);
+		// TODO Auto-generated constructor stub
+		this.DuracionStun=duracionstun;
+		this.CantPlantas=cantplantas;
+	}
+
+	public static double getDuracionStun() {
+		return DuracionStun;
+	}
+
+	public static double getCantPlantas() {
+		return CantPlantas;
+	}
+
+	public void setDuracionStun(int duracionStun) {
+		DuracionStun = duracionStun;
+	}
+
+	public void setCantPlantas(int cantPlantas) {
+		CantPlantas = cantPlantas;
+	}
+	
+
+	@Override
+	public double accept(VisitoI visitor) {
+		// TODO Auto-generated method stub
+		return visitor.visitar(this);
+		
+	}
 
 }
+
