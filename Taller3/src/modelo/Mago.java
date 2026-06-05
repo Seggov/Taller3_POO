dbd23a2db4c89b06f830c7cc7752f7d1e1182b45
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-04 22:20:36 -0400

    Implement Mago class with attributes and methods
    Added properties and methods for Mago class.

--- Taller3/src/modelo/Mago.java
+++ Taller3/src/modelo/Mago.java
@@ -1,5 +1,37 @@
 package modelo;
 
-public class Mago {
+import java.util.ArrayList;
+
+import Lógica.App;
+import Lógica.VisitoI;
+import Lógica.reporVisitor;
+
+public abstract class Mago {
+	private String Nombremago;
+	private ArrayList<String> hechizoNombre;
+	
+	public Mago(String nombremago,ArrayList<String> hechizonombre) {
+		this.Nombremago=nombremago;
+		this.hechizoNombre=hechizonombre;
+	}
+
+	public ArrayList<String> getHechizoNombre() {
+		return hechizoNombre;
+	}
+
+	public void setHechizoNombre(ArrayList<String> hechizoNombre) {
+		this.hechizoNombre = hechizoNombre;
+	}
+
+	public String getNombremago() {
+		return Nombremago;
+	}
+
+	public void setNombremago(String nombremago) {
+		Nombremago = nombremago;
+	}
+	
+	public abstract void accept(reporVisitor save);
 
 }
+
