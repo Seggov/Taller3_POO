83809a288214f8e37249d3e7888fc9fc01ab74d8
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-04 22:00:37 -0400

    Add Fuego class extending Hechizo with burn duration

--- Taller3/src/modelo/HechizoCuego.java
+++ Taller3/src/modelo/HechizoCuego.java
@@ -1,5 +1,35 @@
 package modelo;
 
-public class HechizoCuego {
+import Lógica.VisitoI;
 
+public class Fuego extends Hechizo{
+	private static double duracionQuemadura;
+	
+	public Fuego(String nombrehechizo, String tipohechizo, double daño,int duracionquemadura) {
+		super(nombrehechizo, tipohechizo, daño);
+		// TODO Auto-generated constructor stub
+		this.duracionQuemadura=duracionquemadura;
+	}
+
+	public static double getDuracionQuemadura() {
+		return duracionQuemadura;
+	}
+
+	public void setDuracionQuemadura(int duracionQuemadura) {
+		this.duracionQuemadura = duracionQuemadura;
+	}
+	
+	
+
+	@Override
+	public double  accept(VisitoI visitor) {
+		// TODO Auto-generated method stub
+		return visitor.visitar(this);
+		
+		
+	}
+
+	
+	
 }
+
