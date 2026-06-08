eca8d69fbb296d3aa91c84b4bc2fa0a029867575
valentinacastillo02-hash <valentina.castillo02@alumnos.ucn.cl>
2026-06-07 20:41:27 -0400

    comentarios listo

--- Taller3/src/main/Main.java
+++ Taller3/src/main/Main.java
@@ -5,8 +5,8 @@ import vista.MenuConsola;
 public class Main {
 
     public static void main(String[] args) {
-        MenuConsola menu = new MenuConsola();
-        menu.iniciar();
+        MenuConsola menu = new MenuConsola();//Instancia clase
+        menu.iniciar();//llama al Metodo Iniicar en la clase menu
     }
 }
 
